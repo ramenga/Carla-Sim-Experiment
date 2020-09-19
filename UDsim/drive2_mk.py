83a0bf1848eed8e2fee756e6bb3e4282ff70f539
H Lalramenga <32388233+ramenga@users.noreply.github.com>
2020-09-20 00:27:26 +0530

    Add files via upload

---
+++ UDsim/drive2_mk.py
@@ -0,0 +1,193 @@
+'''
+load model and drive
+'''
+#parsing command line arguments
+import argparse
+#decoding camera images
+import base64
+#for frametimestamp saving
+from datetime import datetime
+#reading and writing files
+import os
+#high level file operations
+import shutil
+#matrix math
+import numpy as np
+#real-time server
+import socketio
+#concurrent networking 
+import eventlet
+#web server gateway interface
+import eventlet.wsgi
+#image manipulation
+from PIL import Image
+#web framework
+from flask import Flask
+#input output
+from io import BytesIO
+
+import tensorflow as tf
+
+#load our saved model
+from keras.models import load_model
+
+#helper class
+import utils2_mk
+import cv2
+
+#initialize our server
+sio = socketio.Server()
+#our flask (web) app
+app = Flask(__name__)
+#init our model and image array as empty
+model = None #the Keras ML model
+prev_image_array = None
+
+#set min/max speed for our autonomous car
+MAX_SPEED = 25
+MIN_SPEED = 10
+
+def keras_process_image(img):
+    image_x = 100
+    image_y = 100
+    img = cv2.resize(img, (image_x, image_y))
+    img = np.array(img, dtype=np.float32)
+    img = np.reshape(img, (-1, image_x, image_y, 1))
+    return img
+
+def keras_predict(model, image):
+    processed = keras_process_image(image)
+    steering_angle = float(model.predict(processed, batch_size=1))
+    steering_angle = steering_angle*60
+    return steering_angle
+
+
+
+#and a speed limit
+speed_limit = MAX_SPEED
+'''
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+my_devices = tf.config.experimental.list_physical_devices(device_type='CPU')
+tf.config.experimental.set_visible_devices(devices= my_devices, device_type='CPU')
+'''
+#registering event handler for the server
+@sio.on('telemetry') #receiving telemetry data from server @ event
+def telemetry(sid, data): #seems to be the inference of the ML (INFERENCE)
+    if data:
+        # The current steering angle of the car
+        steering_angle = float(data["steering_angle"])
+        # The current throttle of the car, how hard to push peddle
+        throttle = float(data["throttle"])
+        # The current speed of the car
+        speed = float(data["speed"])
+        # The current image from the center camera of the car
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        try:
+            image = np.asarray(image)       # from PIL image to numpy array
+            #cv2.imshow("",image)
+            image = utils2_mk.preprocess(image) # apply the preprocessing
+            #image = np.array([image])       # the model expects 4D array
+
+            # predict the steering angle for the image
+            
+            cv2.imshow("",image)
+            degrees = keras_predict(model, image)
+            #steering_angle = float(input("enter steering val:"))
+            #steering_angle = float(model.predict(image))
+            steering_angle = degrees
+            print("\nsteering_angle_norm:", degrees)
+            
+            input_start =14.63#1.0 #14.33
+            input_end = 14.66#12.0 #14.42
+            output_start = -1
+            output_end = 1
+            norm_steer = output_start + ((output_end - output_start) / (input_end - input_start)) * (steering_angle - input_start)
+            steering_angle = norm_steer
+            print("\nsteering_angle_remap[-1,1]:", steering_angle)
+            
+            '''
+            if degrees > 14.39 :
+                steering_angle = 0.7
+            elif degrees < 14.35:
+                steering_angle = -0.7
+            '''
+            cv2.waitKey(1)
+            # lower the throttle as the speed increases
+            # if the speed is above the current speed limit, we are on a downhill.
+            # make sure we slow down first and then go back to the original max speed.
+            global speed_limit
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED  # slow down
+            else:
+                speed_limit = MAX_SPEED
+            throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2
+
+            print('{} {} {}'.format(steering_angle, throttle, speed))
+            send_control(steering_angle, throttle)
+        except Exception as e:
+            print(e)
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+    else:
+        
+        sio.emit('manual', data={}, skip_sid=True)
+
+
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Remote Driving')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+    )
+    args = parser.parse_args()
+
+    #load model
+    model = load_model(args.model) #load model from file 
+
+    if args.image_folder != '': #You can set to record the run or not
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
+
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
